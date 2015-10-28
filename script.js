088209b44f64799846e2f07becaf77a68cd2e7c8
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-10-27 20:26:04 -0400

    Update stuff from key input.

--- script.js
+++ script.js
@@ -1,3 +1,126 @@
+keys = {
+  w: false,
+  a: false,
+  s: false,
+  d: false
+};
+
+x_direction = 0; //positive is right
+y_direction = 0; //positive is forward
+
+motors = [null, null, null, null];
+/*
+[1 2]
+[3 4]
+*/
+
 document.addEventListener("keydown", function(event) {
   console.log(event.which);
+  /*
+  w: 87
+  a: 65
+  s: 83
+  d: 68
+  */
+  switch(event.which) {
+    case 87:
+      key = "w";
+      break;
+    case 65:
+      key = "a";
+      break;
+    case 83:
+      key = "s";
+      break;
+    case 68:
+      key = "d";
+      break;
+    default:
+      return;
+      break;
+  }
+  keys[key] = true;
+  updateStatus();
+  sendStatus();
+  displayInfo();
 });
+document.addEventListener("keyup", function(event) {
+  console.log(event.which);
+  /*
+  w: 87
+  a: 65
+  s: 83
+  d: 68
+  */
+  switch(event.which) {
+    case 87:
+      key = "w";
+      break;
+    case 65:
+      key = "a";
+      break;
+    case 83:
+      key = "s";
+      break;
+    case 68:
+      key = "d";
+      break;
+    default:
+      return;
+      break;
+  }
+  keys[key] = false;
+  updateStatus();
+  sendStatus();
+  displayInfo();
+});
+function updateStatus() {
+  if(keys.w) {
+    ++y_direction;
+  }
+  if(keys.a) {
+    --x_direction;
+  }
+  if(keys.s) {
+    --y_direction;
+  }
+  if(keys.d) {
+    ++x_direction;
+  }
+
+  x = x_direction;
+  y = y_direction;
+  if(x==1 && y==1) {
+    motors = [true, null, true, null];
+  }
+  if(x==1 && y==0) {
+    motors = [true, false, true, false];
+  }
+  if(x==1 && y==-1) {
+    motors = [false, null, false, null];
+  }
+  if(x==0 && y==1) {
+    motors = [true, true, true, true];
+  }
+  if(x==0 && y==0) {
+    motors = [null, null, null, null];
+  }
+  if(x==0 && y==-1) {
+    motors = [false, false, false, false];
+  }
+  if(x==-1 && y==1) {
+    motors = [null, true, null, true];
+  }
+  if(x==-1 && y==0) {
+    motors = [false, true, false, true];
+  }
+  if(x==-1 && y==-1) {
+    motors = [null, false, null, false];
+  }
+}
+function sendStatus() {
+  
+}
+function displayInfo() {
+
+}
