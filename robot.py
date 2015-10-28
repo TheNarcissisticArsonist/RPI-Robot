360e34122c543dacc51725c188c7684ca5674c1b
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-10-28 15:56:24 -0400

    Convert to booleans

--- robot.py
+++ robot.py
@@ -4,3 +4,11 @@ while True:
     if cont != "":
         cont = cont.split()
         print cont #cont is an array of strings
+        for i in range(0, 4):
+            if cont[i] == "true":
+                cont[i] = True
+            elif cont[i] == "false":
+                cont[i] = False
+            else:
+                cont[i] = None
+        print cont #cont is an array of booleans or NoneTypes
